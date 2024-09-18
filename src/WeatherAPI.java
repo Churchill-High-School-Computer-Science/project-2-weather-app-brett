8f82fca9a6141a5da45a8741aff97ba6ae0eadde
Brett MacKay <bmackay2967@stu.neisd.net>
2024-09-18 12:58:44 -0500

    submit

--- src/WeatherAPI.java
+++ src/WeatherAPI.java
@@ -49,10 +49,10 @@ public class WeatherAPI {
     public String parseWeatherData(String blockOfText) {
 
         //TODO
-        String name = getCityName(blockOfText);
-        String temp = getTemp(blockOfText) ;
-        String feel = getFeelsLike(blockOfText);
-        String humid = getHumidity(blockOfText);
+        String name = "Weather in " + getCityName(blockOfText) + ":";
+        String temp = "Temperature: " + getTemp(blockOfText) + "°F";
+        String feel = "feels like: " + getFeelsLike(blockOfText) + "°F";
+        String humid = "Humidity:" + getHumidity(blockOfText) + "%";
         return name + "\n" + temp + "\n" + feel + "\n" + humid;
 
 
